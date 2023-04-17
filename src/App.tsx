c23b54a1c66b54ab86c8f394629b819f22aa689e
Priyam Aneja <priyamaneja2002@gmail.com>
2023-04-17 12:35:17 +0530

    Navbar update
    Added different links, image and effects in navbar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import Navbar from "@/scenes/navbar";
+import { useState } from "react";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<string>("home");
+
   return (
     <div className="app bg-gray-20">
-      <Navbar />
+      <Navbar 
+        selectedPage={selectedPage}  
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   )
 }
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,15 +1,54 @@
 import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
+import Link from "./Link";
 
-type Props = {}
+type Props = {
+    selectedPage: string;
+    setSelectedPage : (value: string) => void;
+};
 
-const Navbar = (props: Props) => {
+const Navbar = ({selectedPage, setSelectedPage}: Props) => {
     const flexBetween = "flex items-center justify-between";
   return (
     <nav>
         <div 
             className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+              <div className={`${flexBetween} mx-auto w-5/6`}>
+                <div className={`${flexBetween} w-full gap-16`}>
+                  {/* {LeftSide} */}
+                  <img src={Logo} alt="logo" />
+
+                  {/* {RightSide} */}
+                  <div className={`${flexBetween} w-full`}>
+                    {/* {InnerLefttSide} */}
+                    <div className={`${flexBetween} gap-8 text-sm`}>
+                      <Link 
+                        page="Home"
+                        selectedPage={selectedPage}  
+                        setSelectedPage={setSelectedPage}  
+                      />
+                      <Link 
+                        page="Benifits"
+                        selectedPage={selectedPage}  
+                        setSelectedPage={setSelectedPage}/>
+                      <Link 
+                        page="Our Classes"
+                        selectedPage={selectedPage}  
+                        setSelectedPage={setSelectedPage}/>
+                      <Link 
+                        page="Contact Us"
+                        selectedPage={selectedPage}  
+                        setSelectedPage={setSelectedPage}/>
+                    </div>
+                    {/* {InnerRightSide} */}
+                    <div className={`${flexBetween} gap-8`}>
+                      <p>Sign In</p>
+                      <button>Become a Member</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
         </div>
     </nav>
