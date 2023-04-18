b78f38cc40c033b4dfa4a2fd1a0edaf91cd69ec7
Priyam Aneja <priyamaneja2002@gmail.com>
2023-04-18 11:18:26 +0530

    Added navbar mobile menu modal
    Added navbar mobile modal, added some style, different color if navbar is not on top of the page

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -7,18 +7,21 @@ import useMediaQuery from "@/hooks/useMediaQuery";
 import ActionButton from "@/shared/ActionButton";
 
 type Props = {
+    isTopOfPage: boolean;
     selectedPage: SelectedPage;
     setSelectedPage : (value: SelectedPage) => void;
 };
 
-const Navbar = ({selectedPage, setSelectedPage}: Props) => {
+const Navbar = ({isTopOfPage, selectedPage, setSelectedPage}: Props) => {
     const flexBetween = "flex items-center justify-between";
     const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
     const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+    const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
+
   return (
     <nav>
         <div 
-            className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+            className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}>
               <div className={`${flexBetween} mx-auto w-5/6`}>
                 <div className={`${flexBetween} w-full gap-16`}>
                   {/* {LeftSide} */}
@@ -61,8 +64,39 @@ const Navbar = ({selectedPage, setSelectedPage}: Props) => {
                     )}
                 </div>
               </div>
-
         </div>
+
+        {/* {Mobile Menu Modal} */}
+        {!isAboveMediumScreens && isMenuToggled && (
+          <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+            {/* {Close Icon} */}
+            <div className="flex justify-end p-12">
+              <button onClick={() => setIsMenuToggled(!isMenuToggled) }>
+                <XMarkIcon className="h-6 w-6 text-gray-400" />
+              </button>
+            </div>
+            {/* {Menu Items} */}
+            <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+              <Link 
+                page="Home"
+                selectedPage={selectedPage}  
+                setSelectedPage={setSelectedPage}  
+              />
+              <Link 
+                page="Benifits"
+                selectedPage={selectedPage}  
+                setSelectedPage={setSelectedPage}/>
+              <Link 
+                page="Our Classes"
+                selectedPage={selectedPage}  
+                setSelectedPage={setSelectedPage}/>
+              <Link 
+                page="Contact Us"
+                selectedPage={selectedPage}  
+                setSelectedPage={setSelectedPage}/>
+            </div>
+          </div>
+        )}   
     </nav>
   )
 }
